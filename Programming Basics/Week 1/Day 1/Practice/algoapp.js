55fdcdd0d1223a5f21b075ecaa0e6e1f77f45e55
Faress1312 <alayafares4@gmail.com>
2023-10-17 17:50:40 +0200

    commit

---
+++ Programming Basics/Week 1/Day 1/Practice/algoapp.js
@@ -0,0 +1,33 @@
+// LeveL 1
+//Activity 1/5
+var x =5
+console.log(x)
+5
+
+//Activity 2/5
+var x = 3
+x = x+1
+console.log(x)
+4
+
+//Activity 3/5
+var x = 3
+x =x+2
+console.log(x+2)
+console.log(x)
+7,5
+
+//Activity 4/5
+var x = 3
+x = x*x
+console.log(x)
+console.log(x*2)
+9,18
+
+//Activity 5/5
+var x = 2
+var y = 5
+var z = x+y
+console.log(z)
+7
+
