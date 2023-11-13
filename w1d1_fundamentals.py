01c85fff97b9d4ca11503bae81e9eb23cb782731
Faress1312 <alayafares4@gmail.com>
2023-11-13 13:59:14 +0100

    commit

---
+++ w1d1_fundamentals.py
@@ -0,0 +1,14 @@
+# ctrl + /
+    """
+    this is multiline comment 
+    DOCSTRING
+    """
+    # JS : VAR  variablename = value
+    first__variable = "Hello word"
+    """Js :camelCase
+    Python : snake_case
+    C# / Java : pascalCase
+    """
+    #Global_VARIABLE
+    APP_Name = "E-COMMERCE"
+    PROT = 5000 
