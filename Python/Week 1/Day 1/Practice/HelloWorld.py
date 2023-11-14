4898ba138259f34353e93d5b661c471524890be9
Faress1312 <alayafares4@gmail.com>
2023-11-14 17:02:08 +0100

    commit

--- Python/Week 1/Day 1/Practice/HelloWorld.py
+++ Python/Week 1/Day 1/Practice/HelloWorld.py
@@ -18,11 +18,11 @@
 print("hello world")
 
 
-name= "iheb"
+name= "fares"
 print("hello" , name)
 
 
-name="iheb"
+name="FARES"
 print("hello " + name)
 
 
@@ -33,8 +33,8 @@ num=7
 print("hello " + str(num))
 
 
-fave_food1 = "sushi"
-fave_food2 = "pizza"
-print( f"i love to eat{fave_food1} and {fave_food2}" .format(fave_food1="sushi",fave_food2="pizza"))
-print( f"i love to eat{fave_food1} and {fave_food2}")
+fave_food1 = " sushi"
+fave_food2 = "metabga"
+print( f"i love to eat {fave_food1} and {fave_food2}" .format(fave_food1="sushi",fave_food2="metabga"))
+print( f"i love to eat {fave_food1} and {fave_food2}")
 
