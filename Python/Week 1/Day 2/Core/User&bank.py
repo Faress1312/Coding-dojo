5200d5129271ff0f97bb21ae3b7739257dc26688
Faress1312 <alayafares4@gmail.com>
2023-11-15 20:18:21 +0100

    commit

--- Python/Week 1/Day 2/Core/User&bank.py
+++ Python/Week 1/Day 2/Core/User&bank.py
@@ -35,27 +35,7 @@ class BankAccount:
         return None
 
 
-# class User:
-#     def __init__(self, name, email):
-#         self.name = name
-#         self.email = email
-#         self.account= BankAccount(int_rate=0.02, balance=0)
-    
-#     # other methods
-    
-#     def make_deposit(self, amount):
-#     # your code here
-#         self.account.deposit(amount)
-#         return self
-#     def make_withdraw(self,amount):
-#         self.account.withdraw(amount)
-#         return self
-#     def display_user_balance(self) :
-#         self.account.display_account_info()
-#         return self
 
-# user1=User("ramzi","alayafares")    
-# user1.display_user_balance()
 acc1=BankAccount(0.01,30)
 acc2=BankAccount()
 
