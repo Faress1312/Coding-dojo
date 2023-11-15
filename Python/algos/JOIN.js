688d378721f89f0eed94e88741612a63e0f7e18b
Faress1312 <alayafares4@gmail.com>
2023-11-15 10:28:04 +0100

    commit

---
+++ Python/algos/JOIN.js
@@ -0,0 +1,43 @@
+const arr1 = [1, 2, 3];
+const separator1 = ", ";
+const expected1 = "1, 2, 3";
+
+const arr2 = [1, 2, 3];
+const separator2 = "-";
+const expected2 = "1-2-3";
+
+const arr3 = [1, 2, 3];
+const separator3 = " - ";
+const expected3 = "1 - 2 - 3";
+
+const arr4 = [1];
+const separator4 = ", ";
+const expected4 = "1";
+
+const arr5 = [];
+const separator5 = ", ";
+const expected5 = "";
+
+// 1. Driver 🚗
+// 2. Presenter 👩‍💻
+// 3. Navigator 🧭
+
+// take 5-7 mins to write the pseudocode here:
+
+// create the function and decide what params it needs and what it will return
+function join(arr, separator) {
+    var result = ""
+    for(var i =0; )
+}
+
+
+const arr = [1,2,7];
+const separator = "+" ;
+const expected = "" ;
+console.log(arr.join());
+console.log(arr.join("+"));
+console.log(arr.join(""))
+console.log(arr.join(", "))
+console.log(arr.join(""))
+console.log(arr.join("-"))
+
